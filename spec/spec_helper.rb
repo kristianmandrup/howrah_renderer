5ec542e644a3e550b9261728729c331604a21bc5
Kristian Mandrup <kmandrup@gmail.com>
2010-06-25 14:59:00 +0200

    added requires to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ Bundler.setup(:default, :test)
 require 'howrah_renderer'
 require 'rspec'
 require 'rspec/autorun'
+# gems
+require 'prawn_commander'
+require 'kmandrup-colorist'
+require 'proxy_party'
+require 'foxy_factory'
+# rspec helpers
 require 'command_matchers'
 require 'exemplar'
 
